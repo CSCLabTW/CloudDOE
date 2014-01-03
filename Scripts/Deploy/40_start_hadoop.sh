9dcd28c9de36fe1d7c2440b0fe231d4de3148445
moneycat <moneycat.tw@gmail.com>
2014-01-03 11:17:20 +0800

    CBPHI: Fix Hadoop start procedure in 40_hadoop_start.sh.

---
+++ Scripts/Deploy/40_start_hadoop.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+	echo "Using:" $0 "{HadoopDir}"; exit
+else
+	if [ ! -d $1 ]; then
+		echo  "---- [ERROR 4] $1 does not exist! ----"; exit
+	elif [ ! -x $1 ]; then
+		echo  "---- [ERROR 4] $1 permission error! ----"; exit
+	fi
+
+        echo "---- [4] Start Hadoop Cluster ----";
+        if [ ! -d /var/hadoop/hadoop-$(id -u -n)/dfs/name ]; then
+                echo "---- [4.1] Formating Namenode... ----"
+                $1/bin/hadoop namenode -format
+        else
+                echo "---- [4.1] Namenode is formatted. Skip... ----"
+        fi
+
+        echo "---- [4.2] Starting DFS... ----"
+        $1/bin/start-dfs.sh
+        echo "---- [4.1] Starting MapReduce Framework... ----"
+        $1/bin/start-mapred.sh	
+fi
+
+# vim: ai ts=2 sw=2 et sts=2 ft=sh
