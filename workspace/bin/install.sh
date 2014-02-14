00344afb3d98943db0bc7901db85f993d6b3bf13
moneycat <moneycat.tw@gmail.com>
2014-02-14 15:28:40 +0800

    CloudDOE: add license headers in each source file. Closes #2269.

--- workspace/bin/install.sh
+++ workspace/bin/install.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
-
-# Author: Wei-Chun Chung <wcchung@iis.sinica.edu.tw>
 #
-# v0.1: 2012-12-28 for CloudBrush Hadoop Installation
-# v1.0: 2014-01-20 for CloudDOE Deploy
+# (C) Copyright 2013 The CloudDOE Project and others.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+# Contributors:
+#      Wei-Chun Chung (wcchung@iis.sinica.edu.tw)
+#      Yu-Chun Wang (zxaustin@iis.sinica.edu.tw)
+# 
+# CloudDOE Project:
+#      http://clouddoe.iis.sinica.edu.tw/
 #
 # This script is modified from the installation script written by 
 # Jazz Yao-Tsung Wang <jazzwang.tw@gmail.com>.
 # For more information, please check:
 # https://github.com/jazzwang/hicloud-hadoop
+#
 
 WORKSPACE=../../workspace
 CONFIGDIR=../config/common
